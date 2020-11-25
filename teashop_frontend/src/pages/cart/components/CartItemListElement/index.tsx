d11f8ecbe8c97f58bc809a3d93bd8a7e4104d756
Łukasz Ślazyk <lukasz.slazyk@gmail.com>
2020-11-25 12:17:22 +0100

    Fixed styling problems in cart list element

--- teashop_frontend/src/pages/cart/components/CartItemListElement/index.tsx
+++ teashop_frontend/src/pages/cart/components/CartItemListElement/index.tsx
@@ -1,4 +1,4 @@
-import { Fab, Grid, Typography } from "@material-ui/core";
+import { Divider, Fab, Grid, Typography } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import React, { useEffect, useState } from "react";
 import { CartItem } from "../../../../domain/cart/models";
@@ -20,7 +20,7 @@ const CartItemListElement = (props: Props) => {
     const classes = useStyles();
     const [ownInteractionDisabled, setOwnInteractionDisabled] = useState(false);
     const { interactionDisabled } = props;
-    
+
     const handleQuantityChanged = (value: number) => {
         setOwnInteractionDisabled(true);
         props.updateItemQuantityCallback(props.cartItem.product.id, value);
@@ -44,57 +44,62 @@ const CartItemListElement = (props: Props) => {
             spacing={3}
             className={classes.root}
         >
-            <Grid item>
+            <Grid item container className={classes.imageContainer}>
                 <img
                     src={getImageFullUrl(props.cartItem.product.imagePath)}
                     alt="product"
                     className={classes.image}
                 />
             </Grid>
-            <Grid item className={classes.contentContainer}>
-                <Grid container alignItems="center" spacing={3}>
-                    <Grid item className={classes.productNameTextContainer}>
-                        <Typography variant="h6">
-                            {props.cartItem.product.name}
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h6">
-                            {calculateItemPrice(props.cartItem).toFixed(2)} EUR
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Grid
-                            container
-                            alignItems="center"
-                            spacing={3}
-                            className={classes.buttonsContainer}
-                        >
-                            <Grid item>
-                                <ProductQuantityPicker
-                                    initialValue={props.cartItem.quantity}
-                                    pricedByWeight={
-                                        props.cartItem.product
-                                            .quantityPerPrice > 1
-                                    }
-                                    quantityChangedCallback={
-                                        handleQuantityChanged
-                                    }
-                                    quantityInvalidCallback={props.quantityInvalidCallback}
-                                    quantityValidCallback={props.quantityValidCallback}
-                                    interactionDisabled={ownInteractionDisabled}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <Fab
-                                    size="small"
-                                    onClick={handleRemoveButtonClick}
-                                    disabled={ownInteractionDisabled}
-                                    className={classes.removeButton}
-                                >
-                                    <DeleteIcon />
-                                </Fab>
-                            </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid
+                item
+                container
+                spacing={3}
+                className={classes.contentContainer}
+            >
+                <Grid item className={classes.productNameTextContainer}>
+                    <Typography variant="h6">
+                        {props.cartItem.product.name}
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="h6">
+                        {calculateItemPrice(props.cartItem).toFixed(2)} EUR
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Grid
+                        container
+                        alignItems="center"
+                        justify="center"
+                        spacing={3}
+                    >
+                        <Grid item>
+                            <ProductQuantityPicker
+                                initialValue={props.cartItem.quantity}
+                                pricedByWeight={
+                                    props.cartItem.product.quantityPerPrice > 1
+                                }
+                                quantityChangedCallback={handleQuantityChanged}
+                                quantityInvalidCallback={
+                                    props.quantityInvalidCallback
+                                }
+                                quantityValidCallback={
+                                    props.quantityValidCallback
+                                }
+                                interactionDisabled={ownInteractionDisabled}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Fab
+                                size="small"
+                                onClick={handleRemoveButtonClick}
+                                disabled={ownInteractionDisabled}
+                                className={classes.removeButton}
+                            >
+                                <DeleteIcon />
+                            </Fab>
                         </Grid>
                     </Grid>
                 </Grid>
